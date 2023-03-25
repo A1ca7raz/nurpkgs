032c536f4cb4e79f4375451f7db4ae662ed2d90a
a1ca7raz <imc@sent.as>
2023-03-25 14:37:32 +0800

    Nurpkgs: fix pack
    Nurpkgs: fix build

--- .github/push_cache.sh
+++ .github/push_cache.sh
@@ -3,22 +3,30 @@
 extra_args=${ATTIC_PUSH_ARGS:--j8}
 cache=${ATTIC_CACHE:-test}
 retry_times=${ATTIC_PUSH_RETRY:-3}
+[[ $CI_MODE ]] && dryrun_cmd="" || dryrun_cmd=echo
 
 push_with_retry() {
   for n in $(seq 1 ${retry_times}); do
-    attic push ${extra_args} ${cache} $1 && return 0
+    $dryrun_cmd attic push ${extra_args} ${cache} $1 && return 0
   done
   false
 }
 
-[[ $1 ]] && pkglist=($1) || pkglist=($(nix eval .#checks.x86_64-linux --apply 'x: with builtins; map (x: x.outPath) (attrValues x)' --impure | sed -e 's/"//g' -e 's/ /\n/g' | sed -e '1d' -e '$d'))
+[[ $* ]] && pkgs_need_build=$* || pkgs_need_build=
+[[ $pkgs_need_build ]] && filter_expr="{ inherit (x) $pkgs_need_build; }" || filter_expr="x"
+apply_expr="x: with builtins; let pkglist = ${filter_expr}; in map (x: x.outPath) (attrValues pkglist)"
+pkglist=($(nix eval .#checks.x86_64-linux --apply "$apply_expr" --impure | sed -e 's/"//g' -e 's/ /\n/g' | sed -e '1d' -e '$d'))
 
+echo -en "\e[35m==>\e[0m Total ${#pkglist[*]} package"
+[[ ${#pkglist[*]} -gt 1 ]] && echo 's' || echo
+[[ ${#pkglist[*]} == 0 ]] && echo -e "\e[31mERROR:\e[0m No package or wrong packages given." && exit 1
 echo '>>>>>>>>>> Package Store >>>>>>>>>>'
 for i in ${pkglist[*]}; do echo ${i}; done
 echo '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
 
 build_fail=1
 for i in ${pkglist[*]}; do
+  echo
   echo -e " \e[32m=====>> $i\e[0m"
   if push_with_retry $i; then
     echo -en " \e[32m"
