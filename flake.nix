4c3ee8e0aa80743604ca798d94de08c0d11cd1ce
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2023-08-24 10:44:58 +0800

    CI: use nvfetcher flake

--- flake.nix
+++ flake.nix
@@ -4,11 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
 
     # Packages from other flakes
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nvfetcher = {
+      url = "github:berberman/nvfetcher";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-compat.follows = "flake-compat";
     };
   };
 
@@ -24,33 +34,43 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ overlay ];
+          overlays = [
+            overlay
+            inputs.nvfetcher.overlays.default
+          ];
         };
         nurpkgs = import ./. { inherit pkgs; };
         inherit (pkgs) mkShell;
         unfreePkgs = extraPackages pkgs;
+
+        nvfetcher = inputs.nvfetcher.packages.${system}.default;
       in rec {
         legacyPackages = (flake-utils.lib.filterPackages pkgs.system (nurpkgs))
           // unfreePkgs
-          // inputs.sops-nix.packages.${system};
+          // inputs.sops-nix.packages.${system}
+          // inputs.nvfetcher.packages.${system};
         packages = legacyPackages;
         checks = legacyPackages;
         formatter = pkgs.nixpkgs-fmt;
-        devShells.default = mkShell { nativeBuildInputs = with pkgs; [ nvfetcher ]; };
+        devShells.default = mkShell { nativeBuildInputs = [ nvfetcher ]; };
         apps.update = {
           type = "app";
           program = (pkgs.writeShellScript "script" ''
-            ${pkgs.nvfetcher}/bin/nvfetcher -o pkgs/_sources "$@"
+            ${nvfetcher}/bin/nvfetcher -o pkgs/_sources "$@"
           '').outPath;
         };
     }) // {
       overlay = self.overlays.default;
       overlays.default = overlay;
+      overlays.nvfetcher = inputs.nvfetcher.overlays.default;
 
       nixosModule = self.nixosModules.default;
       nixosModules.default = { ... }: {
         imports = utils.importsDirs ./modules;
-        nixpkgs.overlays = [ self.overlay ];
+        nixpkgs.overlays = [
+          self.overlays.default
+          self.overlays.nvfetcher
+        ];
         nix.settings = {
           substituters = [ meta.cache ];
           trusted-public-keys = [ meta.pubkey ];
