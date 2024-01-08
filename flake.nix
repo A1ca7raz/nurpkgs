71105f648e4b6fa573a1eb4989729a149504e64d
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-08 14:13:55 +0800

    add authentik-nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-23-05.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
+    };
+
+    # Dependencies of 3rd-party flakes
+    nixpkgs-lib.url = "github:NixOS/nixpkgs/nixos-unstable?dir=lib";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+    };
+
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    napalm = {
+      url = "github:nix-community/napalm";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     # Packages from other flakes
@@ -19,6 +38,16 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
       inputs.flake-compat.follows = "flake-compat";
+    };
+    authentik-nix = {
+      url = "github:nix-community/authentik-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.flake-parts.follows = "flake-parts";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.nixpkgs-23-05.follows = "nixpkgs-23-05";
+      inputs.poetry2nix.follows = "poetry2nix";
+      inputs.napalm.follows = "napalm";
     };
   };
 
@@ -48,18 +77,21 @@
         unfreePackages = extraPackages pkgs;
         nvfetcherPackages = nvfetcher.packages.${system};
         sopsPackages = inputs.sops-nix.packages.${system};
+        authentikPackages = inputs.authentik-nix.packages.${system};
       in rec {
         legacyPackages = customPackages //
           unfreePackages //
           sopsPackages //
-          nvfetcherPackages;
+          nvfetcherPackages //
+          authentikPackages;
         packages = legacyPackages;
         packageBundles = utils.mkPackageBundles pkgs ./pkgs // {
           inherit
             unfreePackages
             customPackages
             nvfetcherPackages
-            sopsPackages;
+            sopsPackages
+            authentikPackages;
           ciPackages = nvfetcherPackages // sopsPackages;
         };
         checks = legacyPackages;
