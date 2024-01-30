fca7fac96a81949c427e1ac8e147917d204cbb61
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-01-30 15:37:14 +0800

    config: remove self-hosted cache

--- config.nix
+++ config.nix
@@ -10,11 +10,11 @@
   };
 
   substituters = [
-    "https://cache.wtm.moe/test"
+#     "https://cache.wtm.moe/test"
     "https://cache.garnix.io"
   ];
   trusted-public-keys = [
-    "test:k65B9/sD+gF6JX+Ug8ZSzcIbJ/pwD6JpUS/c9sx0SWI="
+#     "test:k65B9/sD+gF6JX+Ug8ZSzcIbJ/pwD6JpUS/c9sx0SWI="
     "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
   ];
 }
