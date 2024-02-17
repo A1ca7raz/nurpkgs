c59e4db080178621df75b3416ab8ca343c508627
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-02-18 01:03:27 +0800

    virtualdesktopbar: update patch

--- pkgs/uncategorized/applet-virtual-desktop-bar-wayland/default.nix
+++ pkgs/uncategorized/applet-virtual-desktop-bar-wayland/default.nix
@@ -7,8 +7,8 @@ libsForQt5.plasma-applet-virtual-desktop-bar.overrideAttrs (p: {
 
   patches = [
     (fetchpatch {
-      url = "https://github.com/wsdfhjxc/virtual-desktop-bar/compare/master...lenonk:virtual-desktop-bar:12384281e68b77ba2911ba0ea2d183bdd6b41170.patch";
-      hash = "sha256-tUIRIx42Fec/X1tEBQV7tqAoFJf+51YG30dtT1OopMg=";
+      url = "https://github.com/wsdfhjxc/virtual-desktop-bar/compare/master...lenonk:virtual-desktop-bar:b6fc41959d95c8cca3d25aa2a201790fa391aab3.patch";
+      hash = "sha256-hflgFDk45eBCa8ilR2D5a+3CoOZliskUuU6nKU3j43Q=";
     })
   ];
 
