2edd38b1de830554571efdd5aebfd42d2c84548d
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-05-18 00:35:06 +0800

    addmod: encrypted_networkd

--- modules/encrypted_cfg/hosts.nix
+++ modules/encrypted_cfg/hosts.nix
@@ -17,17 +17,15 @@ in {
   };
 
   config = mkIf (cfg != {}) {
-    sops.templates.encrypted_hosts = {
-      content =
-        let
-          hosts = filterAttrs (_: v: v != []) cfg;
-          oneToString = set: ip: ip + " " + concatStringsSep " " set.${ip} + "\n";
-          allToString = set: concatMapStrings (oneToString set) (attrNames set);
-        in ''
-          127.0.0.1 localhost
-          ::1 localhost
-        '' + (allToString hosts);
-    };
+    sops.templates.encrypted_hosts.content =
+      let
+        hosts = filterAttrs (_: v: v != []) cfg;
+        oneToString = set: ip: ip + " " + concatStringsSep " " set.${ip} + "\n";
+        allToString = set: concatMapStrings (oneToString set) (attrNames set);
+      in ''
+        127.0.0.1 localhost
+        ::1 localhost
+      '' + (allToString hosts);
 
     environment.etc."hosts".source = mkForce config.sops.templates.encrypted_hosts.path;
 
---
+++ modules/encrypted_cfg/networkd.nix
@@ -0,0 +1,63 @@
+{ lib, config, ... }:
+with lib; let
+  cfg = config.utils.encrypted.networkd;
+in {
+  options.utils.encrypted.networkd = mkOption {
+    type = with types; attrsOf (attrsOf (attrsOf (coercedTo (either int str) (x: [(toString x)]) (listOf str))));
+    default = {};
+    description = ''
+      Definition of systemd networks.
+    '';
+  };
+
+  config = mkIf (cfg != {}) {
+    sops.templates = foldlAttrs
+      (acc: n: v:
+        recursiveUpdate acc (
+          let
+            tplName = "encrypted_networkd_${n}";
+          in {
+            "${tplName}" = {
+              content = foldlAttrs
+                (acc: n: v:
+                  acc + "[${n}]\n"
+                    + foldlAttrs
+                      (acc: n: v:
+                        acc + (concatMapStringsSep "\n" (x: "${n}=${x}") v) + "\n"
+                      )
+                      ""
+                      v
+                    + "\n"
+                )
+                ""
+                v;
+              mode = "0444";
+            };
+          }
+        )
+      )
+      {}
+      cfg;
+    
+    environment.etc = foldlAttrs
+      (acc: n: v:
+        recursiveUpdate acc (
+          let
+            tplName = "encrypted_networkd_${n}";
+            fileName = "systemd/network/${n}.network";
+          in {
+            "${fileName}".source = mkForce config.sops.templates."${tplName}".path;
+          }
+        )
+      )
+      {}
+      cfg;
+
+    systemd.services.systemd-networkd.restartTriggers = mapAttrsToList
+      (n: v: v.content)
+      (filterAttrs
+        (n: v: hasPrefix "encrypted_networkd_" n)
+        config.sops.templates
+      );
+  };
+}
