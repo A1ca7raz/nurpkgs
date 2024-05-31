e5d0108a71563cd637cb51d645e31ffd4089f994
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-05-31 20:06:00 +0800

    encrypted_hosts: add reversedHosts

--- modules/encrypted_cfg/hosts.nix
+++ modules/encrypted_cfg/hosts.nix
@@ -1,22 +1,39 @@
 { lib, config, ... }:
 with lib; let
   cfg = config.utils.encrypted.hosts;
+  cfgr = config.utils.encrypted.reversedHosts;
 in {
-  options.utils.encrypted.hosts = mkOption {
-    type = with types; attrsOf (coercedTo str (x: [x]) (listOf str));
-    default = {};
-    example = literalExpression ''
-      {
-        "127.0.0.1" = [ "foo.bar.baz" ];
-        "192.168.0.2" = [ "fileserver.local" "nameserver.local" ];
-      };
-    '';
-    description = ''
-      Locally defined maps of hostnames to IP addresses.
-    '';
+  options.utils.encrypted = {
+    hosts = mkOption {
+      type = with types; lazyAttrsOf (coercedTo str (x: [x]) (listOf str));
+      default = {};
+      example = literalExpression ''
+        {
+          "127.0.0.1" = [ "foo.bar.baz" ];
+          "192.168.0.2" = [ "fileserver.local" "nameserver.local" ];
+        };
+      '';
+      description = ''
+        Locally defined maps of hostnames to IP addresses.
+      '';
+    };
+
+    reversedHosts = mkOption {
+      type = with types; lazyAttrsOf (coercedTo str (x: [x]) (listOf str));
+      default = {};
+      example = literalExpression ''
+        {
+          "foo.bar.baz" = [ "127.0.0.1" ];
+          "fileserver.local" = [ "127.0.0.1" "::1" ];
+        };
+      '';
+      description = ''
+        Locally defined maps of hostnames to IP addresses but hostname as the key.
+      '';
+    };
   };
 
-  config = mkIf (cfg != {}) {
+  config = mkIf (cfg != {} || cfgr != {}) {
     sops.templates.encrypted_hosts.mode = "0444";
     sops.templates.encrypted_hosts.content =
       let
@@ -34,13 +51,29 @@ in {
         };
         oldhosts = filterHosts config.networking.hosts;
         hosts = filterHosts cfg;
+        hostsr = filterHosts cfgr;
 
-        mergedHosts = mergeHosts (mergeHosts hosts localhost) oldhosts;
+        finalHosts = mergeHosts (mergeHosts hosts localhost) oldhosts;
 
-        oneToString = set: ip: ip + " " + concatStringsSep " " set.${ip} + "\n";
-        allToString = set: concatMapStrings (oneToString set) (attrNames set);
+        finalHostsStr =
+          let
+            oneToString = set: ip: ip + " " + concatStringsSep " " set.${ip} + "\n";
+          in
+            concatMapStrings (oneToString finalHosts) (attrNames finalHosts);
+
+        reversedHostsStr =
+          let
+            oneToString = set: host:
+              builtins.foldl'
+                (acc: ip:
+                  ip + " " + host + "\n" + acc
+                )
+                ""
+                set."${host}";
+          in
+            concatMapStrings (oneToString hostsr) (attrNames hostsr);
       in
-        (allToString mergedHosts) + config.networking.extraHosts;
+        finalHostsStr + reversedHostsStr + config.networking.extraHosts;
 
     environment.etc."hosts".source = mkForce config.sops.templates.encrypted_hosts.path;
 
