2d5d68560b0030a027071fc53e0d828052dba839
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-07-20 15:53:57 +0800

    chromium: add wayland support

--- pkgs/uncategorized/ungoogled-chromium-drm/default.nix
+++ pkgs/uncategorized/ungoogled-chromium-drm/default.nix
@@ -3,4 +3,11 @@
 }:
 ungoogled-chromium.override {
   enableWideVine = true;
+  commandLineArgs = [
+    "--ozone-platform-hint=auto"  # Native Wayland
+    "--enable-wayland-ime"  # Fcitx5
+
+    # Touchpad Gestures for Navigation, Vaapi, Vulkan
+    "--enable-features=TouchpadOverscrollHistoryNavigation,VaapiVideoDecodeLinuxGL,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
+  ];
 }
