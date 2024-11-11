fb6fc1120887aa8959ec73101dc7ec780f728be4
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-11-11 12:32:42 +0800

    baidupcs: follow upstream

--- pkgs/uncategorized/baidupcs-go/default.nix
+++ pkgs/uncategorized/baidupcs-go/default.nix
@@ -1,20 +1,7 @@
 {
   source,
-  lib,
-  buildGoModule
+  baidupcs-go
 }:
-buildGoModule {
-  inherit (source) pname version src;
-  vendorHash = "sha256-msTlXtidxLTe3xjxTOWCqx/epFT0XPdwGPantDJUGpc=";
-  doCheck = false;
-
-  postInstall = ''
-    ln -s $out/bin/BaiduPCS-Go $out/bin/baidupcs-go
-  '';
-
-  meta = with lib; {
-    description = "iikira/BaiduPCS-Go 原版基础上集成了分享链接/秒传链接转存功能";
-    homepage = "https://github.com/qjfoidnh/BaiduPCS-Go";
-    license = licenses.asl20;
-  };
-}
+baidupcs-go.overrideAttrs (p: {
+  inherit (source) version src;
+})
