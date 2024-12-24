41a451e33e329c9aaa152d267c4438383f823f21
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2024-12-24 17:13:56 +0800

    baidupcs: fix

--- pkgs/uncategorized/baidupcs-go-git/default.nix
+++ pkgs/uncategorized/baidupcs-go-git/default.nix
@@ -1,11 +1,8 @@
 {
-  source,
   baidupcs-go
 }:
 baidupcs-go.overrideAttrs (p: {
-  inherit (source) src;
-
   postInstall = p.postInstall + ''
-    ln -s $out/bin/Baidupcs-go $out/bin/baidupcs-go
+    ln -s $out/bin/Baidupcs-Go $out/bin/baidupcs-go
   '';
 })
