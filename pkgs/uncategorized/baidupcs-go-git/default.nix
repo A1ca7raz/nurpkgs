d7a251d8464b633778b2004485d74462109a9f3e
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-01-07 00:15:59 +0800

    baidupcs: fix

--- pkgs/uncategorized/baidupcs-go-git/default.nix
+++ pkgs/uncategorized/baidupcs-go-git/default.nix
@@ -3,6 +3,6 @@
 }:
 baidupcs-go.overrideAttrs (p: {
   postInstall = p.postInstall + ''
-    ln -s $out/bin/Baidupcs-Go $out/bin/baidupcs-go
+    ln -s $out/bin/BaiduPCS-Go $out/bin/baidupcs-go
   '';
 })
