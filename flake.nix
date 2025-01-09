b55461c80a28b40bfaf3ed3c85d5af6e81dd68ac
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-01-09 20:13:33 +0800

    nixpkgs: update stable to 24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-23-05.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
