64328e1d133fc37d6c9c4fd89b8443e3d20e11dc
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-02-27 02:07:30 +0800

    modpkg: merge openssl opensc and ykcs

--- pkgs/uncategorized/openssl-sc/default.nix
+++ pkgs/uncategorized/openssl-sc/default.nix
@@ -1,6 +1,7 @@
 {
   openssl,
   opensc,
+  yubico-piv-tool,
   libp11,
   writeText
 }:
@@ -14,11 +15,18 @@ openssl.override {
 
     [engine_section]
     pkcs11 = pkcs11_section
+    ykcs11 = ykcs11_section
 
     [pkcs11_section]
     engine_id = pkcs11
     dynamic_path = ${libp11}/lib/engines/libpkcs11.so
     MODULE_PATH = ${opensc}/lib/opensc-pkcs11.so
+    init = 0
+
+    [ykcs11_section]
+    engine_id = ykcs11
+    dynamic_path = ${libp11}/lib/engines/libpkcs11.so
+    MODULE_PATH = ${yubico-piv-tool}/lib/libykcs11.so
     init = 0
   '';
 }
