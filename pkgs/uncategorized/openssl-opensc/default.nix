37303d147c44dca502850c96cbc73c2a556a8a01
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-02-26 21:48:36 +0800

    addpkg: openssl-opensc

---
+++ pkgs/uncategorized/openssl-opensc/default.nix
@@ -0,0 +1,24 @@
+{
+  openssl,
+  opensc,
+  libp11,
+  writeText
+}:
+openssl.override {
+  conf = writeText "openssl.cnf" ''
+    openssl_conf = openssl_init
+
+    [openssl_init]
+    engines = engine_section
+    ssl_conf = ssl_module
+
+    [engine_section]
+    pkcs11 = pkcs11_section
+
+    [pkcs11_section]
+    engine_id = pkcs11
+    dynamic_path = ${libp11}/lib/engines/libpkcs11.so
+    MODULE_PATH = ${opensc}/lib/opensc-pkcs11.so
+    init = 0
+  '';
+}
