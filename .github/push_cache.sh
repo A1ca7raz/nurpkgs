97e975ec864864489af7ae05b77edca1959b2fb4
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-03-19 19:16:28 +0800

    CI: update push script

--- .github/push_cache.sh
+++ .github/push_cache.sh
@@ -15,41 +15,21 @@ push_with_retry() {
   false
 }
 
-# Parse opts
-pkgs=()
-groups=()
-for i in $*; do
-  if [[ $(nix eval --raw .#packageBundles.${arch}.${i}.type --impure 2> /dev/null) = "derivation" ]]; then
-    pkgs+=(${i})
-  else
-    groups+=(${i})
-  fi
-done
-
 # Calc store
-pkglist=
-if [[ $groups ]]; then
-  filter_expr="{ inherit (x) ${groups[@]}; }"
-  apply_expr="x: with builtins; let pkglist = ${filter_expr}; in map (x: map (y: y.outPath) (attrValues x)) (attrValues pkglist)"
-  pkglist+=($(nix eval .#packageBundles.${arch} --apply "$apply_expr" --impure | sed -e 's/"//g' -e 's/\[//g' -e 's/\]//g' -e 's/ /\n/g'))
-fi
-
-[[ $pkgs || $groups ]] && filter_expr="{ inherit (x) ${pkgs[@]}; }" || filter_expr="x"
-apply_expr="x: with builtins; let pkglist = ${filter_expr}; in map (x: x.outPath) (attrValues pkglist)"
-pkglist+=($(nix eval .#packages.${arch} --apply "$apply_expr" --impure | sed -e 's/"//g' -e 's/ /\n/g' | sed -e '1d' -e '$d'))
+store_list=($(nix eval --raw .#packages.${arch} --apply "x: with builtins; let store_list=with x;[$*]; in foldl' (acc: y: acc+\" \"+y.outPath) '''' store_list"))
 
 # Show package information
-echo -en "\e[35m==>\e[0m Total \e[35m${#pkglist[*]}\e[0m package"
-[[ ${#pkglist[*]} -gt 1 ]] && echo 's' || echo
+echo -en "\e[35m==>\e[0m Total \e[35m${#store_list[*]}\e[0m package"
+[[ ${#store_list[*]} -gt 1 ]] && echo 's' || echo
 
-[[ ${#pkglist[*]} == 0 ]] && echo -e "\e[31mERROR:\e[0m No package or wrong packages given." && exit 1
+[[ ${#store_list[*]} == 0 ]] && echo -e "\e[31mERROR:\e[0m No package or wrong packages given." && exit 1
 echo '>>>>>>>>>> Package Store >>>>>>>>>>'
-for i in ${pkglist[*]}; do echo ${i}; done
+for i in ${store_list[*]}; do echo ${i}; done
 echo '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'
 
 # Start push
 fail=1
-for i in ${pkglist[*]}; do
+for i in ${store_list[*]}; do
   echo
   echo -e " \e[32m=====>> $i\e[0m"
   if push_with_retry $i; then
