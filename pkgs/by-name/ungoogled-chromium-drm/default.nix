26f2d85cfaf2e0bddcf3bce4dbf4086c37fb0307
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-03-29 01:20:13 +0800

    pkg(chromium): fix shortcut popup

--- pkgs/by-name/ungoogled-chromium-drm/default.nix
+++ pkgs/by-name/ungoogled-chromium-drm/default.nix
@@ -9,5 +9,8 @@ ungoogled-chromium.override {
 
     # Touchpad Gestures for Navigation, Vaapi, Vulkan
     "--enable-features=TouchpadOverscrollHistoryNavigation,VaapiVideoDecodeLinuxGL,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
+
+    # FIXME: Disable popup shortcut setting window
+    "--disable-features=GlobalShortcutsPortal"
   ];
 }
