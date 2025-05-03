6de0be8578bb788f958ff5e9fde230759d8ec831
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-05-03 09:54:33 +0800

    flake: remove garnix cache

--- flake.nix
+++ flake.nix
@@ -64,12 +64,12 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.garnix.io"
+      # "https://cache.garnix.io"
       "https://a1ca7raz-nur.cachix.org"
     ];
 
     extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "a1ca7raz-nur.cachix.org-1:twTlSh62806B8lfG0QQzge4l5srn9Z8/xxyAFauOZnQ="
     ];
   };
