add0dd6fa7b7b905f8553d85e99717cc0e436519
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2025-11-30 00:05:58 +0800

    updpkg(chromium): remove extra args

--- pkgs/by-name/ungoogled-chromium-drm/default.nix
+++ pkgs/by-name/ungoogled-chromium-drm/default.nix
@@ -3,16 +3,6 @@
 }:
 (ungoogled-chromium.override {
   enableWideVine = true;
-  commandLineArgs = [
-    "--ozone-platform-hint=auto"  # Native Wayland
-    "--enable-wayland-ime"  # Fcitx5
-
-    # Touchpad Gestures for Navigation, Vaapi, Vulkan
-    "--enable-features=TouchpadOverscrollHistoryNavigation,VaapiVideoDecodeLinuxGL,VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE"
-
-    # FIXME: Disable popup shortcut setting window
-    "--disable-features=GlobalShortcutsPortal"
-  ];
 }).overrideAttrs (p: {
   version = "${p.version}-custom";
 })
