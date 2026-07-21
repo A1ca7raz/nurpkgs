b0f55bfe6e8e43f895b7fe246ec3ad0b35601156
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2026-07-22 01:20:17 +0800

    flakes: add nix-community cache

--- flake-modules/modules.nix
+++ flake-modules/modules.nix
@@ -5,12 +5,14 @@ let
       "https://a1ca7raz-nur.cachix.org"
       "https://cache.numtide.com"
       "https://niri-nix.cachix.org"
+      "https://nix-community.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "a1ca7raz-nur.cachix.org-1:twTlSh62806B8lfG0QQzge4l5srn9Z8/xxyAFauOZnQ="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "niri-nix.cachix.org-1:SvFtqpDcf7Sm1SMJdby1/+Y+6f3Yt3/3PMcSTKPJNJ0="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 in {
