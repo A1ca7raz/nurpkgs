e62e9ec1e38acff186036e0bfcc1a408347fb63b
A1ca7raz <7345998+A1ca7raz@users.noreply.github.com>
2026-07-22 00:58:32 +0800

    rmpkgs: kde related

--- pkgs/by-name/dolphin-nospace/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  kdePackages,
-}:
-kdePackages.dolphin.overrideAttrs (p: {
-  version = "${p.version}-fix-space";
-  __intentionallyOverridingVersion = true;
-
-  patches = [ ./00-dolphin-return-space.patch ];
-})
